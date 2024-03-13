2cf79416ac9eb91882240b8f2b198fee8e43f00f
founion <154479081+founion@users.noreply.github.com>
2024-03-13 11:03:36 +0900

    Update datasets.py

--- recognition/datasets.py
+++ recognition/datasets.py
@@ -7,8 +7,7 @@ cam.set(4, 480)
 face_detector1 = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 face_detector2 = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
 
-### 추가 ###
-face_num = input('\n enter user id_num end press <return> ==> ')  # 사람에 따라 번호 부여
+face_num = input('\n enter user id_num end press <return> ==> ')  # 사람에 맞는 인덱스 부여
 face_id = input('\n enter user id end press <return> ==> ')  # 저장할 파일 이름 설정
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 
@@ -22,14 +21,13 @@ while True:  # 정면 사진 50장 저장
     for (x,y,w,h) in faces1:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
         count1 += 1
-        ### 변경 ###
-        cv2.imwrite("/home/yk/openvino_notebooks/datasets/test/" + str(face_id) + '_' + "f" + '_' + str(face_num) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imwrite("/home/yk/openvino_notebooks/datasets/test/" + str(face_id) + '_' + "f" + '_' + str(face_num) + '_' + str(count1) + ".jpg", gray[y:y+h,x:x+w])
         cv2.imshow('image', img)
 
     k = cv2.waitKey(100) & 0xff
     if k == 27:
         break
-    elif count1 >= 50:
+    elif count1 >= 50: # 저장할 data 개수 설정
         break
 
 while True:  # 측면(좌) 사진 50장 저장
@@ -39,14 +37,13 @@ while True:  # 측면(좌) 사진 50장 저장
     for (x,y,w,h) in faces2:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
         count2 += 1
-        ### 변경 ###
-        cv2.imwrite("/home/yk/openvino_notebooks/datasets/test/" + str(face_id) + '_' + "pl" + '_'  + str(face_num) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imwrite("/home/yk/openvino_notebooks/datasets/test/" + str(face_id) + '_' + "pl" + '_'  + str(face_num) + '_' + str(count2) + ".jpg", gray[y:y+h,x:x+w])
         cv2.imshow('image', img)
 
     k = cv2.waitKey(100) & 0xff
     if k == 27:
         break
-    elif count2 >= 50:
+    elif count2 >= 50: # 저장할 data 개수 설정
         break
 
 print("\n [INFO] Exiting Program and cleanup stuff")
